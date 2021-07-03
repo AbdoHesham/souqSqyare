b1d4e962d2e0fd67494338be72e9190d2d7bae26
MohamedSalah111500 <mohamedsalah111500@gmail.com>
2021-07-03 07:23:25 +0300

    refresh bug 3.7.6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -42,7 +42,7 @@ import {LocationStrategy, HashLocationStrategy} from '@angular/common';
 
   ],
   providers: [
-      {  provide:LocationStrategy&& HTTP_INTERCEPTORS, useClass:HashLocationStrategy&& InterceptorInterceptor , multi: true },
+      {  provide:LocationStrategy , useClass:HashLocationStrategy , multi: true },
       AuthGuard
 
   ],
